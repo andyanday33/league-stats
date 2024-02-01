7965d84f3200f3541de571b1cf8fa00777c16381
andyanday33 <andyanday33@gmail.com>
2024-02-01 19:12:43 +0000

    feat(backend): add dbcontext, configure pgsql, add summoner model

---
+++ backend/LeagueStatsAPI/Persistence/SummonerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class SummonerDbContext: DbContext
+{
+    public SummonerDbContext(DbContextOptions<SummonerDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Summoner> Summoners { get; set; } = null!;
+}
