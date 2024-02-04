87521b004dcd2bb3f1ac8093bb40fd4ce3512e6a
andyanday33 <andyanday33@gmail.com>
2024-02-04 15:41:52 +0000

    feat(frontend): add summoner name checks

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,11 +11,25 @@ type Props = {};
 export const Navbar = (props: Props) => {
   const pathname = usePathname();
 
+  const [summonerName, setSummonerName] = React.useState("");
+  const [summonerNameError, setSummonerNameError] = React.useState("");
+  const summonerNameRegex = new RegExp(".+#.+", "g");
+
+  const [server, setServer] = React.useState("NA1");
+
   const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleSummonerSearch = () => {
+    if (summonerNameRegex.test(summonerName)) {
+      console.log("Search for summoner: ", summonerName);
+    } else {
+      console.log("Invalid summoner name");
+    }
+  };
 
   return (
     <div className="w-full h-20">
-      <nav className="flex items-center justify-between h-full px-8 max-w-[90rem]">
+      <nav className="flex items-center justify-between h-full px-8 max-w-[90rem] mx-auto">
         <ul className="flex h-full items-center">
           <li className="flex items-center px-4 h-full group hover:bg-gradient-to-t from-leagueGray-100/25 to-leagueGray-hextech-black to-55%">
             <p className="text-2xl font-bold text-text-primary/75 group-hover:text-text-primary">
@@ -83,17 +97,20 @@ export const Navbar = (props: Props) => {
                   onBlur={() => setIsFocused(false)}
                   tabIndex={1}
                   className="bg-transparent text-text-primary px-2 focus:outline-leagueGold-400 focus:border-leagueGold-400 focus:shadow-leagueGold-400"
+                  onChange={(e) => setServer(e.target.value)}
+                  value={server}
                 >
-                  <option>NA</option>
-                  <option>EUW</option>
-                  <option>EUNE</option>
-                  <option>KR</option>
-                  <option>JP</option>
-                  <option>BR</option>
-                  <option>LAN</option>
-                  <option>RU</option>
-                  <option>TR</option>
-                  <option>OCE</option>
+                  <option value="NA1">NA</option>
+                  <option value="EUW1">EUW</option>
+                  <option value="EUN1">EUNE</option>
+                  <option value="KR">KR</option>
+                  <option value="JP1">JP</option>
+                  <option value="BR1">BR</option>
+                  <option value="LA1">LAN</option>
+                  <option value="RU">RU</option>
+                  <option value="TR1">TR</option>
+                  <option value="OC1">OCE</option>
+                  {/* TODO: add remaining servers */}
                 </select>
                 <input
                   onFocus={() => setIsFocused(true)}
@@ -101,6 +118,8 @@ export const Navbar = (props: Props) => {
                   tabIndex={2}
                   type="text"
                   placeholder="Search for a Summoner"
+                  value={summonerName}
+                  onChange={(e) => setSummonerName(e.target.value)}
                   className="bg-transparent p-4 text-text-primary placeholder:text-text-primary/75 focus:outline-leagueGold-400 focus:border-leagueGold-400 focus:shadow-leagueGold-400"
                 />
                 <button
@@ -108,6 +127,7 @@ export const Navbar = (props: Props) => {
                   onBlur={() => setIsFocused(false)}
                   tabIndex={3}
                   className="p-4 group/button hover:bg-gradient-to-t from-leagueGray-100/25 to-leagueGray-hextech-black to-55%"
+                  onClick={handleSummonerSearch}
                 >
                   <MagnifyingGlassIcon className="w-6 h-6 text-text-primary/75 group-hover/button:text-text-primary" />
                 </button>
